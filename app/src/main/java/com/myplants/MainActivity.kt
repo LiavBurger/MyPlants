a1f0cbb268027fcf52ab5169cf0aa085334b60c1
Liav Burger <liav.burger22@Gmail.com>
2024-01-22 22:46:35 +0200

    Added button for navigation between home and signup for testing

--- app/src/main/java/com/myplants/MainActivity.kt
+++ app/src/main/java/com/myplants/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.myplants
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -15,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         val navController = Navigation.findNavController(this, R.id.host_fragment)
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+        // Setup the button click listener
+        val btnToRegister = findViewById<Button>(R.id.btnToRegister)
+        btnToRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

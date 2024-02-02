2d2cde1e52e06937ee4cf2132a8931c3596d3de0
Liav Burger <liav.burger22@Gmail.com>
2024-02-03 01:39:36 +0200

    pictures now show for community too

--- app/src/main/java/com/myplants/adapter/CommunityPlantAdapter.kt
+++ app/src/main/java/com/myplants/adapter/CommunityPlantAdapter.kt
@@ -3,16 +3,19 @@ package com.myplants.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.myplants.R
 import com.myplants.model.Plant
+import com.squareup.picasso.Picasso
 
 class CommunityPlantAdapter(private var plants: List<Plant>) : RecyclerView.Adapter<CommunityPlantAdapter.CommunityPlantViewHolder>() {
 
     class CommunityPlantViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val plantNameTextView: TextView = view.findViewById(R.id.tv_plant_name)
         val plantTypeTextView: TextView = view.findViewById(R.id.tv_plant_type)
+        val plantImageView: ImageView = view.findViewById(R.id.plantImage)
         // You can also add an ImageView for plant image when you have plant images
     }
 
@@ -25,6 +28,7 @@ class CommunityPlantAdapter(private var plants: List<Plant>) : RecyclerView.Adap
         val plant = plants[position]
         holder.plantNameTextView.text = plant.name
         holder.plantTypeTextView.text = plant.type
+        Picasso.get().load(plant.imageUrl).into(holder.plantImageView)
         // Set the plant image if available
     }
 
